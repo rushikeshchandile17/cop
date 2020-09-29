0f820341b41b420dfdfc50eb8fd24819eb32a674
rushikeshchandile17 <53510826+rushikeshchandile17@users.noreply.github.com>
2020-09-29 22:44:53 +0530

    Add files via upload

---
+++ add2numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, s;
+    
+    cout << "Enter two integers: ";
+    cin >> n1 ;
+      cout << "Enter two integers: ";
+       cin >> n2;
+
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    s = n1 + n2;
+
+    // Prints sum 
+    cout << n1 << " + " <<  n2 << " = " << s;     
+
+    return 0;
+}
